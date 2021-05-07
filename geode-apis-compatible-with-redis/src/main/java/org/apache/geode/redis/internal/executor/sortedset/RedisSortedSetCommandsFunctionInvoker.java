4e8ec285bb24b89363787fe77960c27d7075c32c
Ray Ingles <ringles@vmware.com>
2021-05-07 13:58:00 -0400

    WIP tests and implementation, but serialization broken

--- geode-apis-compatible-with-redis/src/main/java/org/apache/geode/redis/internal/executor/sortedset/RedisSortedSetCommandsFunctionInvoker.java
+++ geode-apis-compatible-with-redis/src/main/java/org/apache/geode/redis/internal/executor/sortedset/RedisSortedSetCommandsFunctionInvoker.java
@@ -47,4 +47,10 @@ public class RedisSortedSetCommandsFunctionInvoker extends RedisCommandsFunction
   public byte[] zscore(RedisKey key, byte[] member) {
     return invokeCommandFunction(key, ZSCORE, member);
   }
+
+  @Override
+  public byte[] zscore(RedisKey key, byte[] member) {
+    System.out.println("function invoker about to invoke zscore");
+    return invokeCommandFunction(key, ZSCORE, member);
+  }
 }
