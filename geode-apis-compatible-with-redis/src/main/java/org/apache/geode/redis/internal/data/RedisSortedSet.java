c56bd2e4cbe86a63021a1b9e5b7a3d7ab9de4d6c
Ray Ingles <ringles@vmware.com>
2021-05-13 10:36:21 -0400

    WIP almost all test working

--- geode-apis-compatible-with-redis/src/main/java/org/apache/geode/redis/internal/data/RedisSortedSet.java
+++ geode-apis-compatible-with-redis/src/main/java/org/apache/geode/redis/internal/data/RedisSortedSet.java
@@ -78,7 +78,8 @@ public class RedisSortedSet extends AbstractRedisData {
   }
 
   // for serialization
-  public RedisSortedSet() {}
+  public RedisSortedSet() {
+  }
 
   @Override
   protected void applyDelta(DeltaInfo deltaInfo) {
